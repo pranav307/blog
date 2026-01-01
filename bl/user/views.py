8332486c88f89900bb1e88f1a56e947d3e4484ca
pranav307 <psgusain307@gmail.com>
2026-01-01 22:00:54 +0530

    p

--- bl/user/views.py
+++ bl/user/views.py
@@ -201,15 +201,15 @@ class LikePostview(viewsets.ModelViewSet):
    
 class Commentpagination(PageNumberPagination):
     page_size=2
-    page_query_param="page_size" 
+    page_query_param="page" 
     max_page_size=30
-    def get_paginated_response(self, data):
-        return Response({
-            "count": self.page.paginator.count,
-            "next": self.get_next_link(),
-            "previous": self.get_previous_link(),
-            "results": data,
-        })
+    # def get_paginated_response(self, data):
+    #     return Response({
+    #         "count": self.page.paginator.count,
+    #         "next": self.get_next_link(),
+    #         "previous": self.get_previous_link(),
+    #         "results": data,
+    #     })
 class Commentview(viewsets.ModelViewSet):
     queryset=Comment.objects.all()
     serializer_class=Commentserializer
@@ -269,7 +269,7 @@ class Commentview(viewsets.ModelViewSet):
         replies_count=Count("replies")
         )
 
-        page = self.paginate_queryset(queryset)
+        page = self.paginate_queryset(queryset)  #this will return current model instace in array form
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
@@ -278,7 +278,21 @@ class Commentview(viewsets.ModelViewSet):
     
    
         
+    #   def paginate_queryset(self, queryset, request, view=None):
+    # page_size = self.get_page_size(request)
+    # if not page_size:
+    #     return None
 
+    # paginator = Paginator(queryset, page_size)
+    # page_number = self.get_page_number(request, paginator)
+
+    # try:
+    #     self.page = paginator.page(page_number)
+    # except InvalidPage:
+    #     raise NotFound("Invalid page")
+
+    # self.request = request
+    # return list(self.page)
 
 ###
 #post list view
