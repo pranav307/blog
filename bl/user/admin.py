1c044bca349380d94554cfe5ba96689e8d3db438
pranav307 <psgusain307@gmail.com>
2026-01-12 20:38:48 +0530

    fk

--- bl/user/admin.py
+++ bl/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customuser, Postarticle, LikePost, Comment, Mediahandle
 
-# Register your models here.
+admin.site.register(Customuser)
+admin.site.register(Postarticle)
+admin.site.register(LikePost)
+admin.site.register(Comment)
+admin.site.register(Mediahandle)
